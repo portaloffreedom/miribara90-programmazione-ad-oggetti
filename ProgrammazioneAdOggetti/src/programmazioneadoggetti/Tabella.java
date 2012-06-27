cd30deba1d4bd6a33e14a7880f6bf800c223fe7b
miribara90@gmail.com <miribara90@gmail.com@5dde66aa-3299-83f2-8447-332f82674768>
2012-06-27 09:57:17 +0000

    Hello World Creata finestra principale

---
+++ ProgrammazioneAdOggetti/src/programmazioneadoggetti/Tabella.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programmazioneadoggetti;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author miriam
+ */
+public class Tabella extends JTable {
+
+    public Tabella() {
+        super(20, 10);
+    }
+    
+}
